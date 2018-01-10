a1421cf1f8eda1c598dbf946e1800d9e1de8d579
Amir Kamran <amir@taus.net>
2018-01-10 07:31:53 +0100

    updates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let bodyParser = require('body-parser');
 let config = require('config');
 let cron = require('node-cron');
 let rs = require('./api/crons/replicationScheduler');
+var loginController = require('./api/controllers/loginController');
 let morgan = require('morgan');
 
 let app = express();
@@ -19,9 +20,33 @@ app.use(bodyParser.json());
 // - testing
 require('./api/routes')(app, null, config);
 
+async function initialize(db, config) {
+	await db.listCollections({name: "item"})
+    		.next(function(err, collinfo) {
+    			if (collinfo) {
+    				console.log(collinfo);
+    			} else {
+    				db.createCollection('item');
+    			}
+    });
+	await db.listCollections({name: "user"})
+		.next(function(err, collinfo) {
+			if (collinfo) {
+				console.log(collinfo);
+			} else {
+				db.createCollection('user');
+				loginController.doLogin(null, null, db, config);
+			}
+	});	
+	
+}
+
 MongoClient.connect(config.db.url, function(err, database) {
 	if (err) { return console.log(err); }
+	
 	require('./api/routes')(app, database, config);
+	initialize(database, config);
+		
 	cron.schedule('*/3 * * * * *', function() {
 		rs.run(database, config, null);
 	});
